e7ce64d04001a063e3693c01cf0c618e5254b167
Patrick <patrick.elzinga14@yahoo.com>
2021-03-11 23:27:29 -0500

    Changed Some URLs

--- src/UserData.js
+++ src/UserData.js
@@ -57,8 +57,9 @@ class UserData{
     }
     sendData(url, method, parameters, callback){
         const localstorage_user = JSON.parse(localStorage.getItem('user'));
-        if(localstorage_user !== undefined){
-            const inMemoryToken = localstorage_user.token;
+        let inMemoryToken;
+        if(localstorage_user !== null){
+            inMemoryToken = localstorage_user.token;
         }
 
         return (fetch(url, {
